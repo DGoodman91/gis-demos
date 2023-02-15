e752509057ee503b7d8a5859e3e2c938b5ea7f83
GoodmanDev <david.g.goodman@bath.edu>
2023-02-15 10:53:29 +0000

    ng app continued

--- frontend/src/app/maps/co2bycountry/co2bycountry.component.ts
+++ frontend/src/app/maps/co2bycountry/co2bycountry.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { environment } from '../../../environments/environment.prod';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-co2bycountry',
@@ -6,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./co2bycountry.component.scss']
 })
 export class Co2bycountryComponent {
+
+  style = 'mapbox://styles/mapbox/light-v11';
+
+  constructor() { }
+
+  ngOnInit() {
+    const map = new mapboxgl.Map({
+      accessToken: environment.mapbox.accessToken,
+      container: 'map',
+      style: this.style,
+      center: [0, 0], // initial map center in [lon, lat]
+      zoom: 2,
+      attributionControl: false
+    });
+
+//    map.getStyle().layers.forEach(function (element: { id: string; }, index: any, array: any) {
+//      if (element.id != 'country-label' && element.id != 'continent-label') {
+//        map.removeLayer(element.id);
+//      }
+//    });
+
+  }
 
 }
